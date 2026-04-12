17ddbe6ed7f00166c46edf51869294f29c2f7f95
R-Alejandro <alejandrorhiguera@gmail.com>
2026-04-12 16:04:22 -0500

    1 two sum solution

---
+++ LeetAlejoCode/Easy/TwoSum.cs
@@ -0,0 +1,27 @@
+namespace LeetAlejoCode.Easy;
+
+public class TwoSum
+{
+    // O(n)
+    public int[] Soluiton(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int partner = target - nums[i];
+
+            if (map.ContainsKey(partner))
+            {
+                return new int[] {map[partner], i};
+            }
+            
+            if (!map.ContainsKey(nums[i]))
+            {
+                map.Add(nums[i], i);
+            }
+        }
+
+        return new int[2];
+    }
+}
--- LeetAlejoCode/Program.cs
+++ LeetAlejoCode/Program.cs
@@ -1,2 +1,7 @@
-﻿
-Console.WriteLine("leetcode garbage");
+﻿using LeetAlejoCode.Easy;
+
+Console.WriteLine("#### Easy Peasy Problems ####");
+TwoSum twoSum = new TwoSum();
+int[] test = twoSum.Soluiton([2, 7, 11, 15], 9);
+
+Console.WriteLine($"[{test[0]} {test[1]}]");
