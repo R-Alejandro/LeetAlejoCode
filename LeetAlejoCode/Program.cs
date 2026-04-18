c2b6df968074ba099a791333dcea47e237a265ec
R-Alejandro <alejandrorhiguera@gmail.com>
2026-04-18 17:16:38 -0500

    clean program cs

--- LeetAlejoCode/Program.cs
+++ LeetAlejoCode/Program.cs
@@ -1,21 +1,7 @@
 ﻿using LeetAlejoCode.Easy;
 
 Console.WriteLine("#### Easy Peasy Problems ####");
-// TwoSum twoSum = new TwoSum();
-// int[] test = twoSum.Soluiton([2, 7, 11, 15], 9);
-//
-// Console.WriteLine($"[{test[0]} {test[1]}]");
 
-
-// bool test9 = PalindromeNumber.IsPalindrome(523);
-// Console.WriteLine($"is palindrome?: {test9}");
-
-
-// int test = RomanToInteger.RomanToInt("MCMXCIV");
-// Console.WriteLine($"{test}");
-
-// string common = LongestCommonPrefix.Solution(["dog","racecar","car"]);
-// Console.WriteLine(common);
 
 int result = AddDigits.Solution(4827);
 Console.WriteLine($"{result}");
